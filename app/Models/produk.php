295c8555c91fb8241dbeb52f7466be17ff197ee1
Naaaa3120 <krisnatimb@gmail.com>
2023-03-31 10:44:10 +0800

    ubah typo

--- app/Models/produk.php
+++ app/Models/produk.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class produk extends Model
+class Produk extends Model
 {
     use HasFactory;
 
-    protected $table ='Produk';
-    
+    protected $table ='produk';
+
 
     protected $fillable =[
         'nama',

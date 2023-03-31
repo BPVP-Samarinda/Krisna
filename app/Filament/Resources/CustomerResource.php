8498244276be848b5ed712ab15e9b37cff21bd43
Naaaa3120 <krisnatimb@gmail.com>
2023-03-31 11:48:47 +0800

    perbaikan customer

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -29,8 +29,8 @@ class CustomerResource extends Resource
                 Forms\Components\DatePicker::make('tanggal_lahir'),
                 Forms\Components\Radio::make('jenis_kelamin')
                 ->options([
-                    'P' => 'Perempuan',
-                    'L' => 'Laki - Laki',
+                    'Perempuan' => 'P',
+                    'Laki - Laki' => 'L',
                 ]),
 
                 Forms\Components\TextInput::make('telpon'),
